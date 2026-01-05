f4d7f9f96bd1c5fae057b01719acb0b7788c9f9e
baejinwoo <baejinwoo@wisefour.com>
2026-01-05 11:32:04 +0900

    deploy

--- assets/minimize-B-5VTQQ4.js
+++
@@ -1,1 +0,0 @@
-import{c as t,o,a as n}from"./index-D41IXjbz.js";const r={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",viewBox:"0 0 24 24"};function s(a,e){return o(),t("svg",r,[...e[0]||(e[0]=[n("path",{d:"M6 19h12v2H6v-2z"},null,-1)])])}const c={render:s};export{c as default,s as render};
